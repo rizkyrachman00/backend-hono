e19e2fa8ae89f0d70b47c553b9238e21e194e8f6
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-06-07 12:40:00 +0700

    chore: modify eslint config (add rules eslint)

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,18 @@ export default antfu({
     quotes: "double",
   },
   ignores: ["**/migrations/*"],
+}, {
+  rules: {
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
 });
