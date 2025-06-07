083fe7123f67a5271aa730d07f77e7fe2c404b97
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-06-08 00:27:08 +0700

    feat: add stoker openAPI helpers

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,25 +1,23 @@
 import { createRoute, z } from "@hono/zod-openapi";
+import * as HttpStatusCode from "stoker/http-status-codes";
+import { jsonContent } from "stoker/openapi/helpers";
+import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 import { createRouter } from "@/lib/create.app.js";
 
 const router = createRouter()
   .openapi(createRoute({
+    tags: ["Index"],
     method: "get",
     path: "/",
     responses: {
-      200: {
-        content: {
-          "application/json": {
-            schema: z.object({
-              message: z.string(),
-            }),
-          },
-        },
-        description: "Get Data index",
-      },
+      [HttpStatusCode.OK]: jsonContent(
+        createMessageObjectSchema("DB API"),
+        "Get Data index",
+      ),
     },
   }), (c) => {
-    return c.json({ message: "DB API" });
+    return c.json({ message: "DB API" }, HttpStatusCode.OK);
   });
 
 export default router;
