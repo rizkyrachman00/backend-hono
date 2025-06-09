81ebd33d64f68fb9f6f239cee92f35acaef344b2
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-06-09 17:26:21 +0700

    feat: create (member) route tags

--- src/routes/members/member.routes.ts
+++ src/routes/members/member.routes.ts
@@ -2,9 +2,12 @@ import { createRoute, z } from "@hono/zod-openapi";
 import * as HTTPStatusCode from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
 
-export  const list = createRoute({
+const tags = ["Members"];
+
+export const list = createRoute({
   path: "/members",
   method: "get",
+  tags,
   responses: {
     [HTTPStatusCode.OK]: jsonContent(
       z.array(z.object({
