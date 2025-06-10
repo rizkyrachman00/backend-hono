058fc2cd1d6069367a9458d7d8cdd5c4695e09f9
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-06-10 12:05:42 +0700

    feat: create query db from member handlers

--- src/routes/members/member.handlers.ts
+++ src/routes/members/member.handlers.ts
@@ -1,9 +1,10 @@
 import type { AppRouteHandler } from "@/lib/types.js";
 
+import db from "@/db/index.js";
+
 import type { ListMemberRoutes } from "./member.routes.js";
 
-export const list: AppRouteHandler<ListMemberRoutes> = (c) => {
-  return c.json([
-    { name: "John Doe", done: false },
-  ]);
+export const list: AppRouteHandler<ListMemberRoutes> = async (c) => {
+  const members = await db.query.members.findMany();
+  return c.json(members);
 };
