1c4964cd17c5933d8f8afd0fa59a656d99d6d7b3
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-06-11 03:16:21 +0700

    feat: customize drizzle-zod schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -15,7 +15,7 @@ export const members = pgTable("members", {
 });
 
 export const membersSelectSchema = createSelectSchema(members);
-export const membersInsertSchema = createInsertSchema(members).omit({ id: true, createdAt: true, updatedAt: true, deletedAt: true });
+export const membersInsertSchema = createInsertSchema(members, { name: schema => schema.name.min(1), email: schema => schema.email.email() }).omit({ id: true, createdAt: true, updatedAt: true, deletedAt: true });
 
 export const branches = pgTable("branches", {
   id: uuid("id").primaryKey().defaultRandom(),
